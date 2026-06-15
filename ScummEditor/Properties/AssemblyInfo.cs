c7e0098ab90405e8967c5f110584a348d31625b0
Matheus Moreira <matheus.moreira@gmail.com>
2026-06-15 16:01:51 -0300

    Bump version to 2.0.0.0 and add changelog
    Major release adding full SCUMM v4 support (Monkey Island 1 floppy EGA/VGA, Loom CD) on top of the v5/v6 editor: room/object image and z-plane export/import, costume decode/import, game text translation, the v4 script disassembler, all room/sound block viewers, the v4 batch graphics export/import, plus the Japanese (SJIS) string-decode fix. Update AssemblyVersion/FileVersion/InformationalVersion to 2.0.0.0, refresh the assembly description (SCUMM 4, 5 and 6) and add the 2.0.0.0 changelog entry to History.txt (shown in the About window).

--- ScummEditor/Properties/AssemblyInfo.cs
+++ ScummEditor/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("SCUMMeditor")]
-[assembly: AssemblyDescription("Utility to view and edit SCUMM 5 and 6 files")]
+[assembly: AssemblyDescription("Utility to view and edit SCUMM 4, 5 and 6 files")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("SCUMMeditor")]
@@ -32,7 +32,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.5.0.0")]
-[assembly: AssemblyFileVersion("1.5.0.0")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
 // Application.ProductVersion (shown in the About window) reads this on modern .NET.
-[assembly: AssemblyInformationalVersion("1.5.0.0")]
+[assembly: AssemblyInformationalVersion("2.0.0.0")]
